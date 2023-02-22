5c91e5fa17c4af2cde11ef35692d2f511ea4b411
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-02-22 13:58:43 +0000

    scaffold does everything but i typed it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  # get 'reviews/index'
+  # get 'reviews/new'
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :restaurants, only: [:index, :show, :new]
+  resources :restaurants
+  resources :restaurants do
+    resources :reviews # only: [:index, :new]
+  end
 end
